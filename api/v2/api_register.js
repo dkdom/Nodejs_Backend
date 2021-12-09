37bf09a2053e02dd550e8a74e0f113211fe450b6
dkdom <dom.sayoudom@gmail.com>
2021-12-09 19:57:28 +0700

    initial commit

--- api/v2/api_register.js
+++ api/v2/api_register.js
@@ -1,18 +1,55 @@
 const express = require("express")
 const router = express.Router()
-const fileType = require("file-type")
-const {v4: uuidv4} = require("uuid")
+const  { fromBuffer } = require("file-type")
 const fs = require("fs")
+const {v4: uuidv4} = require("uuid")
+const db = require("../../db")
+const moment = require("moment")
+var bcrypt = require("bcryptjs")
 
 
 
-router.post("/register", function(req, res){
+router.post("/register", async function(req, res){
      let myFileName = ""
      if(req.body.imageBase64 && req.body.imageBase64 != ""){
 
           myFileName = await base64ToImage(req.body.imageBase64)
           
      }
+
+      // ------ create parametter ------
+      const uuid = uuidv4()
+      const customer_name = req.sanitize(req.body.customer_name)
+      const last_name = req.sanitize(req.body.last_name)
+      const address = req.sanitize(req.body.address)
+      const tel = req.sanitize(req.body.tel)
+      const email = req.sanitize(req.body.email)
+      const password = bcrypt.hashSync(req.body.password, 10)    //req.sanitize(req.body.password)
+      const image = myFileName
+      const created_date = moment().format("YYYY-MM-DD H:m:s")
+
+
+      // ------ insert into database ------
+
+      let sql = "INSERT INTO tbl_customer(uuid, customer_name, last_name, address, tel, email, password, image, created_date) VALUES ?"
+      let values=[[
+          uuid,
+          customer_name,
+          last_name,
+          address,
+          tel,
+          email,
+          password,
+          image,
+          created_date
+      ]]
+
+      db.query(sql, [values], function(err, rs){
+          if(err) throw err;
+
+          res.json({status: "success", message: "Insert success"})
+      })
+
 })
 
 
@@ -22,7 +59,7 @@ async function base64ToImage(data){
      let fileExt = await(getExtImage(data))
      var myFileName = uuidv4() + "." + fileExt
 
-     fs.writeFile(".'uploaded/" + myFileName, data, 'base64', function(err){
+     fs.writeFile("./uploaded/" + myFileName, data, 'base64', function(err){
           if(err){
                console.log(err)
                myFileName = ""
@@ -34,7 +71,7 @@ async function base64ToImage(data){
 }
 
 async function getExtImage(data){
-     const ext = await fileType(Buffer.from(data, 'base64'))
+     const ext = await fromBuffer(Buffer.from(data, 'base64'))
      return ext.ext;
 }
 
